3b303282a61fbb2d55b58e469bc1d807c58721b3
tx4x <baumgart.guenter@gmail.com>
2018-06-29 17:12:44 +0200

    =basic impl.

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,2 @@
-import * as lodash from 'lodash';
-import * as debug from './log';
-import { curry, unless, when, ifElse, isNil } from 'ramda';
-import * as R from 'ramda';
+export * from './main';
 
-// K :: a -> b -> a (constant combinator)
-const K = curry(
-    x => () => x
-);
-const defaultValue = x => when(isNil, K(x));
